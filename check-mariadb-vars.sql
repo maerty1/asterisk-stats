6293f75c2fa61bad24ea71ddd287e4bf41c115c7
maerty1 <maerty@yandex.ru>
2025-12-26 23:59:44 +0300

    Удалены устаревшие файлы, связанные с оптимизацией MariaDB и документацией, включая скрипты, настройки и руководства. Это упрощает структуру проекта и устраняет ненужные зависимости, улучшая общую производительность и поддержку кода.

--- check-mariadb-vars.sql
+++
@@ -1,5 +0,0 @@
--- Проверка доступных переменных MariaDB
-SHOW VARIABLES LIKE '%innodb%thread%';
-SHOW VARIABLES LIKE '%thread_cache%';
-SHOW VARIABLES LIKE '%parallel%';
-SHOW VARIABLES LIKE '%io_capacity%';
